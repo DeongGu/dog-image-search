eae3ea8de3115219bfb4d99d23807a9a356553e3
SeongWoo Shin <ssw4510@gmail.com>
2023-11-21 23:27:17 +0900

    폴더생성 및 경로 변경, 공통 컴포넌트 생성

---
+++ front/src/App.js
@@ -0,0 +1,11 @@
+import Component from "./Component.js";
+import "./style.css";
+
+export default class App extends Component {
+  template() {
+    return `<header class="header"></header>
+    <main class="main"></main>`;
+  }
+
+  mounted() {}
+}
